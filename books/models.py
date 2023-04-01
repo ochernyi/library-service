998d5e40131a1b834dce798af7be5b6569847b0c
Oleh Chornyi <ochernyi04@gmail.com>
2023-04-01 13:42:03 +0300

    Create Book, Borrowing, User models for project

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Enum(models.IntegerChoices):
+        HARD = 0
+        SOFT = 1
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.BooleanField(choices=Enum.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=5)
--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name="borrowings")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="borrowings")
