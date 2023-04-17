f6087e3de75322f9230c562cf88e7524d308d1be
Oleh Chornyi <ochernyi04@gmail.com>
2023-04-17 05:58:31 +0300

    Change user app

--- users/serializers.py
+++ users/serializers.py
@@ -15,6 +15,16 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data: Dict[str, Any]) -> get_user_model():
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, set the password correctly and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.CharField(label=_("Email"), write_only=True)
