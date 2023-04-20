dc35da9d3e04cf33359d15c5d0458149457b87ff
Oleh Chornyi <ochernyi04@gmail.com>
2023-04-20 07:22:27 +0300

    Correct users app

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,30 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("", CreateUserView.as_view(), name="register"),
+    path("login/", views.obtain_auth_token, name="token"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
+
+# from django.urls import path
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+#
+# from users.views import CreateUserView, ManageUserView
+#
+# urlpatterns = [
+#     path("register/", CreateUserView.as_view(), name="create"),
+#     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+#     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+#     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+#     path("me/", ManageUserView.as_view(), name="manage"),
+# ]
+#
+# app_name = "user"
