9083b4f9674092087b0ccd6e6c53aac6d612e5bd
Oleh Chornyi <ochernyi04@gmail.com>
2023-04-23 04:46:02 +0300

    Fix bugs in borrowing app

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -6,24 +6,26 @@ from rest_framework.exceptions import ValidationError
 
 from books.models import Book
 from borrowing.models import Borrowing
-# from borrowing.telegram_alert import send_message_to_channel
+from borrowing.telegram_bot import send_message
 
 
 class BorrowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ("book", "expected_return")
+        fields = ("book", "expected_return_date")
 
     def create(self, validated_data: Dict[str, Any]) -> Borrowing:
         book_update = Book.objects.get(title=validated_data["book"])
         if book_update.inventory > 0:
             book_update.inventory -= 1
             book_update.save()
-            # asyncio.run(
-            #     send_message_to_channel(
-            #         text=f"{validated_data['user']} just borrowed '{validated_data['book']}' and will return it {validated_data['expected_return']}"
-            #     )
-            # )
+
+            asyncio.run(
+                send_message(
+                    text=f"{validated_data['user']} borrowed '{validated_data['book']}' expected return date: {validated_data['expected_return_date']}"
+                )
+            )
+
             return super().create(validated_data)
         raise ValidationError("This book is unavailable")
 
@@ -43,81 +45,3 @@ class BorrowReturnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("book",)
-
-
-
-# from django.db import transaction
-# from rest_framework import serializers
-# from rest_framework.exceptions import ValidationError
-#
-# from books.serializers import BookSerializer
-# from borrowing.models import Borrowing
-#
-#
-# class BorrowingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Borrowing
-#         fields = (
-#             "id",
-#             "book",
-#             "user",
-#             "is_active",
-#             "borrow_date",
-#             "expected_return_date",
-#         )
-#
-#     def validate(self, attrs: dict) -> dict:
-#         data = super(BorrowingSerializer, self).validate(attrs)
-#
-#         if attrs["book"].inventory <= 0:
-#             raise ValidationError("Book inventory is empty")
-#
-#         if not attrs["is_active"]:
-#             raise ValidationError("Borrow already close")
-#
-#         return data
-#
-#     # @transaction.atomic()
-#     # def create(self, validated_data: dict) -> dict:
-#     #     book = validated_data["book"]
-#     #     book.inventory -= 1
-#     #     book.save()
-#     #     return super().create(validated_data)
-#
-#
-# # class BorrowingListSerializer(BorrowingSerializer):
-# #     book = serializers.SlugRelatedField(many=False, read_only=True, slug_field="title")
-# #     user = serializers.ReadOnlyField(source="user.full_name", read_only=True)
-# #     borrow_date = serializers.ReadOnlyField()
-# #
-# #     class Meta:
-# #         model = Borrowing
-# #         fields = (
-# #             "id",
-# #             "user",
-# #             "borrow_date",
-# #             "expected_return_date",
-# #             "book",
-# #             "is_active",
-# #         )
-# #
-# #
-# # class BorrowingDetailSerializer(BorrowingSerializer):
-# #     borrow_date = serializers.ReadOnlyField()
-# #     book = BookSerializer()
-# #     user_full_name = serializers.ReadOnlyField(source="user.full_name", read_only=True)
-# #     user_email = serializers.ReadOnlyField(source="user.email", read_only=True)
-# #     is_active = serializers.BooleanField(default=False)
-# #
-# #     class Meta:
-# #         model = Borrowing
-# #         fields = (
-# #             "id",
-# #             "user_full_name",
-# #             "user_email",
-# #             "borrow_date",
-# #             "expected_return_date",
-# #             "actual_return_date",
-# #             "book",
-# #             "is_active",
-# #         )
--- borrowing/views.py
+++ borrowing/views.py
@@ -31,7 +31,7 @@ class BorrowViewSet(ModelViewSet):
     serializer_class = BorrowSerializer
     queryset = Borrowing.objects.select_related("book", "user")
     pagination_class = BorrowPagination
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self) -> QuerySet:
         """Retrieve the borrows with filters"""
@@ -93,86 +93,3 @@ class BorrowViewSet(ModelViewSet):
     )
     def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         return super().list(request, *args, **kwargs)
-
-
-
-# from drf_spectacular.utils import extend_schema, OpenApiParameter
-# from rest_framework import viewsets
-# from rest_framework.pagination import PageNumberPagination
-#
-# from borrowing.models import Borrowing
-# from borrowing.permissions import IsAdminOrIfAuthenticatedReadOnly
-# from borrowing.serializers import (
-#     BorrowingSerializer,
-#     # BorrowingListSerializer,
-#     # BorrowingDetailSerializer,
-# )
-#
-#
-# class BorrowingPagination(PageNumberPagination):
-#     page_size = 5
-#     page_size_query_param = "page_size"
-#     max_page_size = 100
-#
-#
-# class BorrowingViewSet(viewsets.ModelViewSet):
-#     queryset = Borrowing.objects.all()
-#     serializer_class = BorrowingSerializer
-#
-#     pagination_class = BorrowingPagination
-#     # permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
-#
-#     # def get_serializer_class(self):
-#     #     if self.action in ("list", "retrieve"):
-#     #         return BorrowingListSerializer
-#     #     if self.action == "update":
-#     #         return BorrowingDetailSerializer
-#     #
-#     #     return BorrowingSerializer
-#
-#     # @staticmethod
-#     # def _params_to_ints(qs):
-#     #     """Converts a list of string IDs to a list of integers"""
-#     #     return [int(str_id) for str_id in qs.split(",")]
-#     #
-#     # def get_queryset(self):
-#     #
-#     #     if self.request.user.is_staff:
-#     #         queryset = self.queryset
-#     #     else:
-#     #         queryset = self.queryset.filter(user_id=self.request.user.id)
-#     #
-#     #     user_id = self.request.query_params.get("user_id")
-#     #     is_active = self.request.query_params.get("is_active")
-#     #
-#     #     if user_id:
-#     #         user_ids = self._params_to_ints(user_id)
-#     #         queryset = queryset.filter(user_id__in=user_ids)
-#     #
-#     #     if is_active is not None:
-#     #         if is_active == "true":
-#     #             queryset = queryset.filter(actual_return_date__isnull=True)
-#     #         if is_active == "false":
-#     #             queryset = queryset.filter(actual_return_date__isnull=False)
-#     #
-#     #     return queryset.distinct()
-#     #
-#     # def perform_create(self, serializer):
-#     #     serializer.save(user_id=self.request.user.id)
-#     #
-#     # @extend_schema(
-#     #     parameters=[
-#     #         OpenApiParameter(
-#     #             "user_id",
-#     #             type={"type": "list", "items": {"type": "number"}},
-#     #             description="Filter by user id (ex. ?user_id=1,2)"
-#     #         ),
-#     #         OpenApiParameter(
-#     #             "is_active",
-#     #             type=str,
-#     #             description="Filter by user_id & is_active (ex. ?is_active=(true/false))",
-#     #         ),
-#     #     ]
-#     # )
-#     # def list(self, request, *args, **kwargs):
-#     #     return super().list(request, *args, **kwargs)
