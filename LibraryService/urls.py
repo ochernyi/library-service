97c253d1670fec56212f221e72ff4fa6a3545c27
Oleh Chornyi <ochernyi04@gmail.com>
2023-04-23 04:49:21 +0300

    Add documentation for the project

--- LibraryService/urls.py
+++ LibraryService/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/borrowings/", include("borrowing.urls", namespace="borrowings")),
-    path("users/", include("users.urls", namespace="user")),
+    path("api/user/", include("users.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
